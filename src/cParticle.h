97fa7f32db38e2d26c7bd16fd6d0559f354bbae3
Michael Torp Kaalund <kaalund@gmail.com>
2020-03-21 23:53:27 +0100

    added include generic.h

--- src/cParticle.h
+++ src/cParticle.h
@@ -4,11 +4,16 @@
 #include <OneLoneCoder/olcPixelGameEngine.h>
 
 #include "cPhysicObject.h"
+#include "Generic.h"
 
 class cParticle : public cPhysicsObject
 {
 public:
-    cParticle( olc::vf2d loc ) : cPhysicsObject(loc) {}
+    float fRadius;
+
+    cParticle( olc::vf2d loc ) : cPhysicsObject(loc) {
+        fRadius = randf(4.0f, 10.0f);
+    }
 
     void Update( float fElapsedTime ) override
     {
