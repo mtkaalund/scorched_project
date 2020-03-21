47abedb8f098b7f5aa17ea1def96939461ac151a
Michael Torp Kaalund <kaalund@gmail.com>
2020-03-21 14:21:58 +0100

    added override to class functions

--- src/cParticle.h
+++ src/cParticle.h
@@ -10,13 +10,13 @@ class cParticle : public cPhysicsObject
 public:
     cParticle( olc::vf2d loc ) : cPhysicsObject(loc) {}
 
-    void Update( float fElapsedTime )
+    void Update( float fElapsedTime ) override
     {
         velocity += acceleration;
         location += velocity;
     }
 
-    void Display( olc::PixelGameEngine * engine )
+    void Display( olc::PixelGameEngine * engine ) override
     {       
         engine->DrawCircle(location, 10, olc::RED);
     }
