8313f0bb8a784821506fcff68e3865a804ef8353
Michael Torp Kaalund <kaalund@gmail.com>
2020-03-21 12:39:45 +0100

    First init of game class

---
+++ src/main.cc
@@ -0,0 +1,53 @@
+#include <OneLoneCoder/olcPixelGameEngine.h>
+
+class Scorched_project : public olc::PixelGameEngine
+{
+public:
+	// Constructor
+	Scorched_project()
+	{
+		sAppName = "Scorched project";
+	}
+
+	bool OnUserCreate()
+	{
+		return true;
+	}
+
+	bool OnUserUpdate( float fElapsedTime )
+	{
+		// Game main loop
+
+		Clear(olc::BLUE);
+
+		DrawString({ScreenWidth()/2, ScreenHeight()/2}, "Hello, TF!!!", olc::GREEN);
+
+
+		return true;
+	}
+
+	bool OnUserDestroy()
+	{
+		// Only use if need to destroy some thing
+
+		return true;
+	}
+
+};
+
+
+int main(int argc, char *argv[])
+{
+	Scorched_project game;
+	if (game.Construct(640, 480, 2, 2))
+	{
+		game.Start();
+	}
+	else
+	{
+		std::cerr << "Could'nt create PixelGameEngine" << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
