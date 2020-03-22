3e1f9d781b2f082f7e907f7063745b78e4b78a24
Michael Torp Kaalund <kaalund@gmail.com>
2020-03-23 00:19:16 +0100

    added 1d perlin noise from olc

--- src/Generic.h
+++ src/Generic.h
@@ -16,6 +16,33 @@ T lerp( T v0, T v1, T t )
     return ( 1 - t ) * v0 + t * v1;
 }
 
+// Taken from Perlin Noise Video https://youtu.be/6-0UaeJBumA
+// Also from https://github.com/OneLoneCoder/videos/blob/master/worms/OneLoneCoder_Worms1.cpp
+void PerlinNoise1D( int nCount, float *fSeed, int nOctaves, float fBias, float *fOutput )
+{
+    // Used 1D Perlin Noise
+    for( int x = 0; x < nCount; x++ )
+    {
+        float fNoise    = 0.0f;
+        float fScaleAcc = 0.0f;
+        float fScale    = 1.0f;
+
+        for( int o = 0; o < nOctaves; o++ )
+        {
+            int nPitch = nCount >> o;
+            int nSample1 = (x / nPitch) * nPitch;
+            int nSample2 = (nSample1 + nPitch) % nCount;
+            float fBlend = (float)(x - nSample1) / (float)nPitch;
+            float fSample = lerp<float>(fSeed[nSample1], fSeed[nSample2], fBlend);
+            fScaleAcc += fScale;
+            fNoise += fSample * fScale;
+            fScale /= fBias;
+        }
+
+        fOutput[x] = fNoise / fScaleAcc;
+    }
+}
+
 // C++17 float random generator
 float randf(float a, float b)
 {
