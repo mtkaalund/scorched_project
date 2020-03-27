717dc3ba40969fbcb10b270d714e8abbbb506875
Michael Torp Kaalund <kaalund@gmail.com>
2020-03-27 22:54:13 +0100

    In the process of moving all map generation to cNoise.h

--- src/Generic.h
+++ src/Generic.h
@@ -11,23 +11,23 @@ T const pi = std::acos(-T(1));
 // Linear interpolation
 // https://en.wikipedia.org/wiki/Linear_interpolation
 template <typename T>
-T lerp( T v0, T v1, T t )
+T lerp(T v0, T v1, T t)
 {
-    return ( 1 - t ) * v0 + ( t * v1 );
+    return (1 - t) * v0 + (t * v1);
 }
 
 // Taken from Perlin Noise Video https://youtu.be/6-0UaeJBumA
 // Also from https://github.com/OneLoneCoder/videos/blob/master/worms/OneLoneCoder_Worms1.cpp
-void PerlinNoise1D( int nCount, float *fSeed, int nOctaves, float fBias, float *fOutput )
+void PerlinNoise1D(int nCount, float *fSeed, int nOctaves, float fBias, float *fOutput)
 {
     // Used 1D Perlin Noise
-    for( int x = 0; x < nCount; x++ )
+    for (int x = 0; x < nCount; x++)
     {
-        float fNoise    = 0.0f;
+        float fNoise = 0.0f;
         float fScaleAcc = 0.0f;
-        float fScale    = 1.0f;
+        float fScale = 1.0f;
 
-        for( int o = 0; o < nOctaves; o++ )
+        for (int o = 0; o < nOctaves; o++)
         {
             int nPitch = nCount >> o;
             int nSample1 = (x / nPitch) * nPitch;
@@ -43,6 +43,23 @@ void PerlinNoise1D( int nCount, float *fSeed, int nOctaves, float fBias, float *
     }
 }
 
+// Simplex Noise (should be in the same family as Perlin noise)
+// http://staffwww.itn.liu.se/~stegu/aqsis/aqsis-newnoise/simplexnoise1234.cpp
+// lampda function of fastfloor
+void SimplexNoise1D(int nCount, int *seed, float fFrequency, float *fOutput)
+{
+    float fFreq = 1.0f;
+    
+
+    // for (int x = 0; x < nCount; x++)
+    // //for (int x = 0; x < 4; x++)
+    // {
+
+        
+    // }
+
+
+}
 
 #if defined(_WIN64) || defined(__x86_64__)
 // C++17 float random generator
@@ -66,20 +83,20 @@ int randi(int a, int b)
     return dis(gen);
 }
 #else
-float randf( float a, float b )
+float randf(float a, float b)
 {
     std::random_device rd;
-    
+
     std::mt19937 gen(rd());
     std::uniform_real_distribution<float> dis(a, b);
 
     return dis(gen);
 }
 
-int randi( int a, int b )
+int randi(int a, int b)
 {
     std::random_device rd;
-    
+
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(a, b);
 
