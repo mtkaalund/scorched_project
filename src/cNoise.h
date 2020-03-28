a34c86b4b454471b2e175b1d6b7fd904429d262d
Michael Torp Kaalund <kaalund@gmail.com>
2020-03-28 10:01:01 +0100

    Mostly finished the Noise class

--- src/cNoise.h
+++ src/cNoise.h
@@ -19,16 +19,35 @@ private:
         hash = new int[length];
         for (int i = 0; i < length; i++)
         {
-            hash[i] = randi(0, 255);
+            hash[i] = randi(0, length);
         }
         hashmask = length - 1;
     }
 
-    float Smooth( float t )
+    float Smooth(float t)
     {
-        return t * t * t * (t * ( t * 6.0f - 15.0f) + 10.0f);
+        return t * t * t * (t * (t * 6.0f - 15.0f) + 10.0f);
     }
 
+    float SimplexValue1DValuePart( float fX, int iX)
+    {
+        float x = fX - iX;
+        float f = 1.0f - x * x;
+        float f2 = f * f;
+        float f3 = f * f2;
+        float h = hash[iX & hashmask];
+        return f3 * h;
+    }
+
+    float Simplex1DPart( float fX, int iX)
+    {
+        float x = fX - iX;
+        float f = 1.0f - x * x;
+        float f2 = f * f;
+        float f3 = f * f2;
+        float h = hash[iX & hashmask];
+        return f3 * h;
+    }
 public:
     Noise()
     {
@@ -37,7 +56,7 @@ public:
 
     Noise(int length)
     {
-        CreateHash(length);
+        CreateHash( length );
     }
 
     ~Noise()
@@ -61,10 +80,36 @@ public:
     }
 
     // Noise type "Perlin"
-    float Perlin1D( int nX, float fFrequency)
+    float Perlin1D(int nX, float fFrequency)
+    {
+        float gradients1d[] = {-1, 1};
+        int gradientsMask1d = 1;
+
+        float fX = nX * fFrequency;
+        int i0 = std::floor(fX);
+        float t0 = fX - i0;
+        float t1 = t0 - 1.0f;
+        i0 &= hashmask;
+        int i1 = i0 + 1;
+
+        int g0 = gradients1d[ hash[i0] & gradientsMask1d ];
+        int g1 = gradients1d[ hash[i1] & gradientsMask1d ];
+
+        float v0 = g0 + t0;
+        float v1 = g1 + t1;
+
+        float t = Smooth(t0);
+        return lerp<float>(v0, v1, t) * 0.25f + 0.5f;
+    }
+
+    // Noise Type Simplex
+    float Simplex1D(int nX, float fFrequency )
     {
         float fX = nX * fFrequency;
-        return 0.0f;
+        int ix = std::floor(fX);
+        float sample = Simplex1DPart(fX, ix);
+        sample += Simplex1DPart(fX, ix + 1);
+        return sample * (1.0f / hashmask) - 0.6f;
     }
 };
 
