74691d260498dccd605f9ef3e494df11646e06c4
Michael Torp Kaalund <kaalund@gmail.com>
2020-03-28 10:46:16 +0100

    Comment out olc perlin noise

--- src/Generic.h
+++ src/Generic.h
@@ -16,50 +16,32 @@ T lerp(T v0, T v1, T t)
     return (1 - t) * v0 + (t * v1);
 }
 
-// Taken from Perlin Noise Video https://youtu.be/6-0UaeJBumA
-// Also from https://github.com/OneLoneCoder/videos/blob/master/worms/OneLoneCoder_Worms1.cpp
-void PerlinNoise1D(int nCount, float *fSeed, int nOctaves, float fBias, float *fOutput)
-{
-    // Used 1D Perlin Noise
-    for (int x = 0; x < nCount; x++)
-    {
-        float fNoise = 0.0f;
-        float fScaleAcc = 0.0f;
-        float fScale = 1.0f;
+// // Taken from Perlin Noise Video https://youtu.be/6-0UaeJBumA
+// // Also from https://github.com/OneLoneCoder/videos/blob/master/worms/OneLoneCoder_Worms1.cpp
+// void PerlinNoise1D(int nCount, float *fSeed, int nOctaves, float fBias, float *fOutput)
+// {
+//     // Used 1D Perlin Noise
+//     for (int x = 0; x < nCount; x++)
+//     {
+//         float fNoise = 0.0f;
+//         float fScaleAcc = 0.0f;
+//         float fScale = 1.0f;
 
-        for (int o = 0; o < nOctaves; o++)
-        {
-            int nPitch = nCount >> o;
-            int nSample1 = (x / nPitch) * nPitch;
-            int nSample2 = (nSample1 + nPitch) % nCount;
-            float fBlend = (float)(x - nSample1) / (float)nPitch;
-            float fSample = lerp<float>(fSeed[nSample1], fSeed[nSample2], fBlend);
-            fScaleAcc += fScale;
-            fNoise += fSample * fScale;
-            fScale = fScale / fBias;
-        }
+//         for (int o = 0; o < nOctaves; o++)
+//         {
+//             int nPitch = nCount >> o;
+//             int nSample1 = (x / nPitch) * nPitch;
+//             int nSample2 = (nSample1 + nPitch) % nCount;
+//             float fBlend = (float)(x - nSample1) / (float)nPitch;
+//             float fSample = lerp<float>(fSeed[nSample1], fSeed[nSample2], fBlend);
+//             fScaleAcc += fScale;
+//             fNoise += fSample * fScale;
+//             fScale = fScale / fBias;
+//         }
 
-        fOutput[x] = fNoise / fScaleAcc;
-    }
-}
-
-// Simplex Noise (should be in the same family as Perlin noise)
-// http://staffwww.itn.liu.se/~stegu/aqsis/aqsis-newnoise/simplexnoise1234.cpp
-// lampda function of fastfloor
-void SimplexNoise1D(int nCount, int *seed, float fFrequency, float *fOutput)
-{
-    float fFreq = 1.0f;
-    
-
-    // for (int x = 0; x < nCount; x++)
-    // //for (int x = 0; x < 4; x++)
-    // {
-
-        
-    // }
-
-
-}
+//         fOutput[x] = fNoise / fScaleAcc;
+//     }
+// }
 
 #if defined(_WIN64) || defined(__x86_64__)
 // C++17 float random generator
